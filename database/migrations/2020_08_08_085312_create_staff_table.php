447ca007698a46633e8feb37ac9af1ce86e401fd
Oyeleye Mayowa <oyeleyemayowa24@gmail.com>
2020-08-08 02:03:36 -0700

    Model and Migration created

---
+++ database/migrations/2020_08_08_085312_create_staff_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStaffTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('staff', function (Blueprint $table) {
+            $table->id();
+            $table->string('staff_id');
+            $table->string('title');
+            $table->string('staff_name');
+            $table->string('department');
+            $table->string('phone_no');
+            $table->string('passport');
+            $table->string('signature');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('staff');
+    }
+}
